cdbb1e575d1dc5b657db10edb8332c1c314a82b7
fedechacke <fede_chaque@hotmail.com>
2020-05-23 17:44:00 -0300

    Creadas las clases base

---
+++ Models/Autor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppResenas.Models
+{
+    public class Autor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public List<int> librosXId { get; set }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppResenas.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public List<Resena> resenasFavoritas { get; set; }
+        public List<Libro> librosFavoritos { get; set; }
+        public List<Autor> autoresFavoritos { get; set; }
+
+    }
+}
