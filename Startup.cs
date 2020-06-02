48ebb9111bdc005f0153b4fc22acedda781ece88
fedechacke <fede_chaque@hotmail.com>
2020-06-01 22:35:14 -0300

    Hecha la migracion falta hacer el update database con un connection String valido.

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using AppResenas.Context;
+
 
 namespace AppResenas
 {
@@ -23,6 +26,10 @@ namespace AppResenas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ResenasDataBaseContext>(
+               options =>
+                options.UseSqlServer(Configuration["ConnectionString:ResenasDataBaseConnection"
+            ]));
             services.AddControllersWithViews();
         }
 
